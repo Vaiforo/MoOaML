fa26321471f820e15e25860d17777bc53e4330b6
vaiforic <77671997+Vaiforo@users.noreply.github.com>
2025-03-01 09:25:40 +0600

    ...

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ a = [[1, 2], [2, 3]]
 for i, j in a:
     print(i)
     print(j)
+
+    # if input(
+    #         "Введите 1, если решить способом северо-зпадного угла, иначе задача будет решена методом минимального элмента: ") == "1":
+    #     self.make_table_nw()
+    #     U, V = self.solve_uv_nw(self.table, self.U, self.V)
+    #     print(self.V, self.U)
+    #     print(self.table)
+    #
+    #     self.solve_nw(self.table, U, V, [])
+    # else:
+    # self.make_table_me()
